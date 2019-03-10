67dacdf0c68e28d72c6da17981ccc8207b67d411
Richard Cissee <ric@richard-cissee.de>
2019-03-10 21:03:25 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,51 @@
+
+# Webtrees 2 Custom Module: ⚶ Vesta Webtrees Location Data Provider
+
+This [webtrees](https://www.webtrees.net/) custom module provides (non-GEDCOM) location data collected via webtrees to other modules.
+The project’s website is [cissee.de](https://cissee.de).
+
+## Contents
+
+* [Features](#features)
+* [Download](#download)
+* [Installation](#installation)
+* [License](#license)
+
+### Features<a name="features"/>
+
+* Provides non-GEDCOM location data (map coordinates) collected via webtrees (Control panel > Map > Geographic data) to other modules.
+Thus, users may migrate easily away e.g. from the original places module, without having to re-enter all location data.
+
+### Download<a name="download"/>
+
+* Current version: 1.7.12.1/2.0.0-alpha.5.1
+* Based on and tested with webtrees 1.7.12, may also work with older 1.7.x versions.
+* Also includes a preview for the upcoming webtrees 2.x, tested with webtrees 2.0.0-alpha.5.
+* Requires the ⚶ Vesta Common module ('vesta_common_lib').
+* Download the zipped module, including all related modules, [here](https://cissee.de/vesta.latest.zip).
+* Support, suggestions, feature requests: <ric@richard-cissee.de>
+
+### Installation<a name="installation"/>
+
+* Unzip the files and copy them to the modules_v3 folder of your webtrees installation. All related modules are included in the zip file. It's safe to overwrite the respective directories if they already exist (they are bundled with other custom modules as well), as long as other custom models using these dependencies are also upgraded to their respective latest versions.
+* Enable the main module via Control Panel -> Modules -> Module Administration -> ⚶ Vesta Webtrees Location Data Provider.
+
+### License<a name="license"/>
+
+* **vesta_location_data: a webtrees custom module**
+* Copyright (C) 2019 Richard Cissée
+* Derived from **webtrees** - Copyright (C) 2010 to 2019 webtrees development team.
+* Derived from **webtrees-geneajaubart** - Copyright (C) 2009 to 2016 Jonathan Jaubart.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
---
+++ WebtreesLocationDataModule.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Cissee\Webtrees\Module\WebtreesLocationData;
+
+use Cissee\Webtrees\Hook\HookInterfaces\EmptyIndividualFactsTabExtender;
+use Cissee\Webtrees\Hook\HookInterfaces\IndividualFactsTabExtenderInterface;
+use Vesta\Hook\HookInterfaces\EmptyFunctionsPlace;
+use Vesta\Hook\HookInterfaces\FunctionsPlaceInterface;
+use Cissee\WebtreesExt\FormatPlaceAdditions;
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Module\AbstractModule;
+use Vesta\Model\PlaceStructure;
+use Fisharebest\Webtrees\Module\ModuleCustomInterface;
+use Fisharebest\Webtrees\Location;
+
+class WebtreesLocationDataModule extends AbstractModule implements ModuleCustomInterface, IndividualFactsTabExtenderInterface, FunctionsPlaceInterface {
+	
+	use EmptyIndividualFactsTabExtender;
+	use EmptyFunctionsPlace;
+	
+	private $vesta;
+	
+	public function __construct(string $directory) {
+		$this->vesta = json_decode('"\u26B6"');
+	}
+
+	public function customModuleAuthorName(): string {
+		return 'Richard Cissée';
+	}
+
+	public function customModuleVersion(): string {
+		return '2.0.0-alpha.5.1';
+	}
+	
+	public function customModuleLatestVersionUrl(): string {
+		return 'https://cissee.de';
+	}
+
+	public function customModuleSupportUrl(): string {
+		return 'https://cissee.de';
+	}
+	
+	public function title(): string {
+		return $this->vesta.' '.I18N::translate('Vesta Webtrees Location Data Provider');
+	}
+	
+	public function description(): string {
+		return I18N::translate('A module providing (non-GEDCOM-based) webtrees location data to other modules.');
+	}
+	
+	//returns lat/lon in int format!
+	protected function getLatLon($gedcomName) {
+		$location = new Location($gedcomName);
+		$latitude  = $location->latitude();
+		$longitude = $location->longitude();
+
+		//wtf [0,0] are also regular coordinates
+		if (($latitude != 0) && ($longitude != 0)) {
+			return array($latitude, $longitude);
+		}
+		
+		return null;
+	}
+	
+	//HookInterface: FunctionsPlaceInterface
+	public function hPlacesGetLatLon(PlaceStructure $place) {
+		return $this->getLatLon($place->getGedcomName());
+	}
+	
+	//we shouldn't have to provide this separately ... just use hPlacesGetLatLon directly in tab!
+	public function hFactsTabGetFormatPlaceAdditions(PlaceStructure $place) {
+		$ll = $this->getLatLon($place->getGedcomName());
+		$tooltip = null;
+		if ($ll != null) {
+			$long = array_pop($ll);
+			$lati = array_pop($ll);
+			
+			//shouldn't be necessary here, types expected to be integer already
+			//$lati = trim(strtr($lati, "NSEW,�", " - -. ")); // S5,6789 ==> -5.6789
+			//$long = trim(strtr($long, "NSEW,�", " - -. ")); // E3.456� ==> 3.456
+			
+			$tooltip = 'via location data';
+			$ll = array($lati, $long);
+		}
+		
+		return new FormatPlaceAdditions('', $ll, $tooltip, '', '');		
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////
+}
